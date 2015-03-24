ee6946366150f4b6d0744c19b8be192e54007388
unknown <wozniak.mateusz@interia.eu>
2015-03-24 12:53:21 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿var express = require("express");
+var app = express();
+
+
+app.get('/mysite', function (req,res) {
+    res.send('Hello! This is test of openShift Env!');
+
+})
+
+
+app.listen(5000);
+console.log('Up and running');
